0d855258fa48794001c921c79e7778bb607cdcc6
Daniel Boam <daniel.boam@open-innovations.org>
2022-04-07 19:56:31 +0100

    Add can change stations using params

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 app.secret_key = "not_very_secret"
 @app.route("/bus-times")
 def index():
-    buses = bustimes.getAllInfo(bustimes.allCoachStnStops())
-
+    stops = bustimes.allCoachStnStops() if len(request.args) < 1 else stopsFromString(request.args.get("q"))
+    buses = bustimes.getAllInfo(stops)
+    
     for bus in buses:
         s = ""
         for attr in bus:
@@ -15,4 +16,7 @@ def index():
 
     return render_template("index.html")
 
+
+def stopsFromString(s):
+    return [(stop.split(":")[0],stop.split(":")[1]) for stop in s.split(";")]
 
