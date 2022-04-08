2a6eff2cb0566f08ebc6b73fa1382e8c02321332
Daniel Boam <daniel.boam@open-innovations.org>
2022-04-08 15:40:09 +0100

    Add changed title depending on station

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
 @app.route("/bus-times")
 def busTimes():
     stops = bustimes.allCoachStnStops() if len(request.args) < 1 else stopsFromString(request.args.get("q"))
-    return renderPageFromStops(stops)
+    return renderPageFromStops(stops,"Custom")
 
 @app.route("/leeds-city-bus-station")
 def showPage1():
@@ -20,7 +20,7 @@ def showPage1():
     8:45030227;9:45030228;10:45030229;11:45030230;12:45030231;13:45030232;14:45030233;15:45030234;
     16:45030235;17:45030236;18:45030237;19:45030238;20:45030239;21:45030240;22:45030241""",prefix)
 
-    return renderPageFromStops(stops)
+    return renderPageFromStops(stops,"Leeds City Bus Station")
 
 
 @app.route("/leeds-corn-exchange-bus-point")
@@ -29,73 +29,81 @@ def showPage2():
     stops = stopsFromString("""A:45010684;B:45010683;C:45012980;D:45010684;E:45012982;
     F:45012984;G:45014779;H:45032247;I:45012652""",prefix)
 
-    return renderPageFromStops(stops)
+    return renderPageFromStops(stops,"Leeds Corn Exchange Bus Point")
 
-@app.route("/bramley-centre-bus-station")
-def showPage3():
-    prefix = "Stand "
-    stops = stopsFromString("""A:45012790;B:45012791;C:45012792;D:45012793;E:45012795""",prefix)
-
-    return renderPageFromStops(stops)
 
 @app.route("/leeds-bradford-airport-bus-point")
 def showPage4():
     prefix = "Stand "
     stops = stopsFromString("""1:45025387;2:45025386;3:45028272""",prefix)
 
-    return renderPageFromStops(stops)  
+    return renderPageFromStops(stops,"Leeds Bradford Airport Bus Point")  
 
 @app.route("/leeds-station-interchange")
 def showPage5():
     prefix = "Stand "
     stops = stopsFromString("""A:45025479;B:45025478;C:45025477;D:45011026""",prefix)
 
-    return renderPageFromStops(stops)  
+    return renderPageFromStops(stops,"Leeds Station Interchange")  
 
 @app.route("/leeds-infirmary-street-bus-point")
 def showPage6():
     prefix = "Stand "
     stops = stopsFromString("""A:45025317;B:45025318;C:45032241;D:45032242;E:45032243""",prefix)
 
-    return renderPageFromStops(stops)  
+    return renderPageFromStops(stops,"Leeds Infirmary Street Bus Point")
+
+@app.route("/white-rose-centre-bus-point")
+def showPage12():
+    prefix = "Stand "
+    stops = stopsFromString("""A:45025310;B:45025311;C:45025312;D:45025313;E:45025314;F:45025315;G:45025316;H:45025352""",prefix)
+
+    return renderPageFromStops(stops,"White Rose Centre Bus Point")      
+
+@app.route("/bramley-centre-bus-station")
+def showPage3():
+    prefix = "Stand "
+    stops = stopsFromString("""A:45012790;B:45012791;C:45012792;D:45012793;E:45012795""",prefix)
+
+    return renderPageFromStops(stops,"Bramley Centre Bus Station")    
 
 @app.route("/morley-town-hall-hub")
 def showPage7():
     prefix = "Stand "
     stops = stopsFromString("""A:45010338;B:45010339;C:45010340;D:45010341;E:45010342""",prefix)
 
-    return renderPageFromStops(stops)  
+    return renderPageFromStops(stops,"Morley Town Hall Hub")  
 
 @app.route("/otley-bus-station")
 def showPage8():
     prefix = "Stand "
     stops = stopsFromString("""1:45025305;2:45025306;3:45025305;4:45025308;5:45010478""",prefix)
 
-    return renderPageFromStops(stops)  
+    return renderPageFromStops(stops,"Otley Bus Station")  
 
 @app.route("/pudsey-bus-station")
 def showPage9():
     prefix = "Stand "
     stops = stopsFromString("""A:45026239;B:45026240;C:45026241;D:45026242;E:45026243;F:45026244""",prefix)
 
-    return renderPageFromStops(stops)  
+    return renderPageFromStops(stops,"Pudsey Bus Station")  
 
 @app.route("/seacroft-bus-station")
 def showPage10():
     prefix = "Stand "
     stops = stopsFromString("""A1:45028111;B:45025301;C:45025302;D:45025303;E:45025304""",prefix)
 
-    return renderPageFromStops(stops)  
+    return renderPageFromStops(stops,"Seacroft Bus Station")  
 
 @app.route("/wetherby-bus-station")
 def showPage11():
     prefix = "Stand "
     stops = stopsFromString("""A:45025353;B:45014895""",prefix)
 
-    return renderPageFromStops(stops) 
+    return renderPageFromStops(stops,"Wetherby Bus Station") 
 
 
-def renderPageFromStops(stops):
+def renderPageFromStops(stops,stationName):
     buses = bustimes.getAllInfo(stops)
     
     for bus in buses:
@@ -104,7 +112,7 @@ def renderPageFromStops(stops):
             s = s + "<td>" + attr + "</td>"
         flash(s)
 
-    return render_template("bus-times.html")
+    return render_template("bus-times.html",station=stationName)
 
 
 def stopsFromString(stops,prefix=""):
